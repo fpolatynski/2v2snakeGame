5e0b8c3dac46c110bee9e9ebb95cbd9f1b76147d
Filip Polatynski <fpolatynski@gmail.com>
2023-10-31 23:45:14 +0100

    first commit

--- game_objects.py
+++ game_objects.py
@@ -5,53 +5,117 @@ vec2 = pg.math.Vector2
 
 
 class Snake:
-    def __init__(self, game):
+    def __init__(self, game, color, keys):
+        self.color = color
+        self.l = keys[0]
+        self.r = keys[1]
+        self.u = keys[2]
+        self.d = keys[3]
         self.game = game
         self.size = game.TILE_SIZE
-        self.rect = pg.rect.Rect([0, 0, game.TILE_SIZE - 2, game.TILE_SIZE - 2])
+        self.rect = pg.rect.Rect([0, 0,
+                                  game.TILE_SIZE - 2,
+                                  game.TILE_SIZE - 2])
         self.rect.center = self.get_random_position()
         self.direction = vec2(0, 0)
         self.step_delay = 100
         self.time = 0
+        self.lenght = 1
+        self.segments = []
+        self.directions = {self.l: 1, self.r: 1, self.u: 1, self.d: 1}
+
+    def check_food(self):
+        if self.rect.center == self.game.food.rect.center:
+            self.game.food.rect.center = self.get_random_position()
+            self.lenght += 5
+
+    def check_border(self):
+        if self.rect.left < 0 or self.rect.right > self.game.WINDOW_SIZE:
+            self.game.new_game()
+        if self.rect.top < 0 or self.rect.bottom > self.game.WINDOW_SIZE:
+            self.game.new_game()
+
+    def check_if_collision(self):
+        if len(self.segments) > len(set([segment.center for segment in self.segments])):
+            self.game.new_game()
 
     def control(self, event):
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_d:
+            if event.key == self.r and self.directions[self.r]:
                 self.direction = vec2(self.size, 0)
+                self.directions = {self.l: 0, self.r: 1, self.u: 1, self.d: 1}
+            if event.key == self.u and self.directions[self.u]:
+                self.direction = vec2(0, -self.size)
+                self.directions = {self.l: 1, self.r: 1, self.u: 1, self.d: 0}
                 
-            if event.key == pg.K_w:
-                self.direction = vec2(0, -self.size) 
-                
-            if event.key == pg.K_a:
+            if event.key == self.l and self.directions[self.l]:
                 self.direction = vec2(-self.size, 0)
+                self.directions = {self.l: 1, self.r: 0, self.u: 1, self.d: 1}
                 
-            if event.key == pg.K_s:
+            if event.key == self.d and self.directions[self.d]:
                 self.direction = vec2(0, self.size)
-    
+                self.directions = {self.l: 1, self.r: 1, self.u: 0, self.d: 1}
+
+    def control2(self, event):
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_RIGHT and self.directions[pg.K_d]:
+                self.direction = vec2(self.size, 0)
+                self.directions = {pg.K_d: 1, pg.K_w: 1, pg.K_a: 0, pg.K_s: 1}
+
+            if event.key == pg.K_UP and self.directions[pg.K_w]:
+                self.direction = vec2(0, -self.size)
+                self.directions = {pg.K_d: 1, pg.K_w: 1, pg.K_a: 1, pg.K_s: 0}
+
+            if event.key == pg.K_LEFT and self.directions[pg.K_a]:
+                self.direction = vec2(-self.size, 0)
+                self.directions = {pg.K_d: 0, pg.K_w: 1, pg.K_a: 1, pg.K_s: 1}
+
+            if event.key == pg.K_DOWN and self.directions[pg.K_s]:
+                self.direction = vec2(0, self.size)
+                self.directions = {pg.K_d: 1, pg.K_w: 0, pg.K_a: 1, pg.K_s: 1}
+
     def delta_time(self):
-        time_now =pg.time.get_ticks()
+        time_now = pg.time.get_ticks()
         if time_now - self.time > self.step_delay:
             self.time = time_now
             return True
         return False
 
     def get_random_position(self):
-        return [randrange(self.size//2, self.game.WINDOW_SIZE -self.size//2, self.size)] * 2
+        return [randrange(self.size//2,
+                          self.game.WINDOW_SIZE -self.size//2,
+                          self.size)] * 2
     
     def move(self):
         if self.delta_time():
             self.rect.move_ip(self.direction)
+            self.segments.append(self.rect.copy())
+            self.segments = self.segments[-self.lenght:]
 
     def update(self):
         self.move()
+        self.check_food()
+        self.check_border()
+        self.check_if_collision()
 
     def draw(self):
-        pg.draw.rect(self.game.screen, 'green', self.rect)
+        [pg.draw.rect(self.game.screen, self.color, segment) for segment in self.segments]
+        pg.draw.rect(self.game.screen, self.color, self.rect)
 
 
 class Food:
     def __init__(self, game):
-        pass
+            self.game = game
+            self.size = game.TILE_SIZE
+            self.rect = pg.rect.Rect([0, 0,
+                                      game.TILE_SIZE - 2,
+                                      game.TILE_SIZE - 2])
+            self.rect.center = self.get_random_position()
+
+    def get_random_position(self):
+        return [randrange(self.size//2,
+                          self.game.WINDOW_SIZE -self.size//2,
+                          self.size)] * 2
 
     def draw(self):
-        pass
+        pg.draw.rect(self.game.screen, (255, 0, 0), self.rect)
--- main.py
+++ main.py
@@ -7,36 +7,40 @@ class Game:
     def __init__(self):
         pg.init()
         self.WINDOW_SIZE = 800
-        self.TILE_SIZE = 20
+        self.TILE_SIZE = 25
         self.screen = pg.display.set_mode([self.WINDOW_SIZE] * 2)
         self.clock = pg.time.Clock()
         self.new_game()
 
     def draw_grid(self):
-        [pg.draw.line(self.screen, (0, 0, 255), (x, 0),(x, self.WINDOW_SIZE)) for x in range(0, self.WINDOW_SIZE, self.TILE_SIZE)]
-        [pg.draw.line(self.screen, (0, 0, 255), (0, y),(self.WINDOW_SIZE, y)) for y in range(0, self.WINDOW_SIZE, self.TILE_SIZE)]
+        [pg.draw.line(self.screen, (255, 182, 193), (x, 0), (x, self.WINDOW_SIZE)) for x in range(0, self.WINDOW_SIZE, self.TILE_SIZE)]
+        [pg.draw.line(self.screen, (255, 182, 193), (0, y), (self.WINDOW_SIZE, y)) for y in range(0, self.WINDOW_SIZE, self.TILE_SIZE)]
 
     def new_game(self):
-        self.snake = go.Snake(self)
+        self.snake1 = go.Snake(self, (255, 20, 147), [pg.K_a, pg.K_d, pg.K_w, pg.K_s])
+        self.snake2 = go.Snake(self, (0,255,0), [pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN])
         self.food = go.Food(self)
 
     def update(self):
         pg.display.flip()
         self.clock.tick(60)
-        self.snake.update()
+        self.snake1.update()
+        self.snake2.update()
 
     def draw(self):
-        self.screen.fill((0,0,200))
+        self.screen.fill((255,182,193))
         self.draw_grid()
         self.food.draw()
-        self.snake.draw()
+        self.snake1.draw()
+        self.snake2.draw()
 
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            self.snake.control(event)
+            self.snake1.control(event)
+            self.snake2.control(event)
 
     def run(self):
         while True:
